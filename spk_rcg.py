e5fee679dbdcd4f48b27144417cd3e54809aa0a0
William Wang <shanhewang@gmail.com>
2018-03-11 14:49:35 -0500

    added bob for UBM-MAP-SVM

--- spk_rcg.py
+++ spk_rcg.py
@@ -2,6 +2,8 @@
 
 import numpy as np
 import scipy.io.wavfile as wav
+import bob.learn.em
+import bob.learn.libsvm
 
 from sklearn.mixture import GaussianMixture
 from python_speech_features import mfcc
@@ -14,14 +16,14 @@ class SPEAKER(object):
         self.GMM_UBM = None
 
     def extract_feature(self):
-            (rate, sig) = wav.read(self.speaker_dataDirectory)
-            mfcc_feat = np.array(mfcc(sig, rate))
+        (rate, sig) = wav.read(self.speaker_dataDirectory)
+        mfcc_feat = np.array(mfcc(sig, rate))
 
-            # Feature normalization, mean subtraction
-            mean = np.mean(mfcc_feat, axis=0)
-            mfcc_feat = mfcc_feat - mean
-            
-            return mfcc_feat
+        # Feature normalization, mean subtraction
+        mean = np.mean(mfcc_feat, axis=0)
+        mfcc_feat = mfcc_feat - mean
+        
+        return mfcc_feat
 
 class GMM_UBM(object):
     def __init__(self, num_comp, spk, bck):
@@ -38,6 +40,38 @@ class GMM_UBM(object):
         scores = np.array(self.GMM.score_samples(data) - self.UBM.score_samples(data))
         socres_sum = np.clip(scores, 0, None).sum()
         return socres_sum
+
+class UBM(object):
+    def __init__(self, num_comp, bck):
+        self.UBM = bob.learn.em.GMMMachine(num_comp, bck.shape[1])
+        self.UBM_Trainer = bob.learn.em.ML_GMMTrainer(True, True, True)
+        bob.learn.em.train(self.UBM_Trainer, self.UBM, bck, max_iterations=200, convergence_threshold=1e-5)
+
+    def get_mean(self):
+        return self.UBM.means
+
+
+class UBM_MAP(object):
+    def __init__(self, ubm, spk):
+        self.MAP = bob.learn.em.GMMMachine(ubm.shape[1], spk.shape[1])
+        self.MAP_Trainer = bob.learn.em.MAP_GMMTrainer(ubm, relevance_factor=4)
+        bob.learn.em.train(self.MAP_Trainer, self.MAP, spk, max_iteration=200, convergence_threshold=1e-5)
+
+    def get_mean(self):
+        return self.MAP.means
+
+    def get_supervector(self):
+        return self.MAP.mean_supervector
+
+class SVM(object):
+    def __init__(self, training_data, data_label):
+        self.trainer = bob.learn.libsvm.Trainer()
+        self.machine = self.trainer.train(training_data)
+        self.label = data_label
+
+    def predict(self, test_data):
+        label_index = self.machine(test_data)
+        return self.label[label_index]
 
 class PREDICTOR(object):
     def __init__(self, spkrs):
